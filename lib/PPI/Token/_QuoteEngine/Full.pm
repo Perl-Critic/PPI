e4c22dc3a8c82285f15974da3b73ba796fe087ba
Chris Dolan <chris+github@chrisdolan.net>
2006-11-20 05:53:54 +0000

    Typo: modifieds -> modifiers

--- lib/PPI/Token/_QuoteEngine/Full.pm
+++ lib/PPI/Token/_QuoteEngine/Full.pm
@@ -42,7 +42,7 @@ BEGIN {
 		# The final ( and kind of depreciated ) "first match only" one is not
 		# used yet, since I'm not sure on the context differences between
 		# this and the trinary operator, but its here for completeness.
-		'?'   => { operator => undef, braced => 0,     separator => '?',   _sections => 1, modifieds => 1 },
+		'?'   => { operator => undef, braced => 0,     separator => '?',   _sections => 1, modifiers => 1 },
 		);
 }
 
