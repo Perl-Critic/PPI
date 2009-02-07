eabcc8ea28245f3b652d3f06fd9f2ec8b2d36a35
Elliot Shank <elliotjs@cpan.org>
2009-02-07 21:52:07 +0000

    Small documentation change to PPI::Statement::Include.

--- lib/PPI/Statement/Include.pm
+++ lib/PPI/Statement/Include.pm
@@ -245,9 +245,10 @@ sub version {
 =pod
 
 The C<arguments> method gives you the rest of the statement after the the
-module/pragma and module version, i.e. the stuff that will eventually be
-passed to the module's C<import()> subroutine.  This does include the comma,
-etc. operators, but doesn't include non-significant direct children.
+module/pragma and module version, i.e. the stuff that will be used to
+construct what gets passed to the module's C<import()> subroutine.  This does
+include the comma, etc. operators, but doesn't include non-significant direct
+children or any final semicolon.
 
 =begin testing arguments 19
 
