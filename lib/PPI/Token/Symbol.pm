4d778ff54d7835e4dcb6ca64efdace439ad2bace
Tom Wyant <wyant@cpan.org>
2011-01-27 22:09:15 +0000

    RT #65199 - Cast can trump braces in PPI::Token::Symbol->symbol()
    Make the PPI::Token::Symbol symbol() method take immediately-preceding
    casts into account when determining which sigil to place on the returned
    value. Without this, PPI claims that '$$foo{bar}' makes use of %foo.
    Actually this is equivalent to the more normal $foo->{bar}, as
    B::Deparse demonstrates.
    
    Casts to arrays (e.g. '@$foo[ 0 .. 2 ]]) are also handled by this patch.

--- lib/PPI/Token/Symbol.pm
+++ lib/PPI/Token/Symbol.pm
@@ -82,6 +82,8 @@ Returns the symbol as a string.
 
 =cut
 
+my %cast_which_trumps_braces = map { $_ => 1 } qw{ $ @ };
+
 sub symbol {
 	my $self   = shift;
 	my $symbol = $self->canonical;
@@ -95,10 +97,18 @@ sub symbol {
 	my $after  = $self->snext_sibling;
 	return $symbol unless _INSTANCE($after, 'PPI::Structure');
 
-	# Process the rest for cases where it might actually be somethign else
+	# Process the rest for cases where it might actually be something else
 	my $braces = $after->braces;
 	return $symbol unless defined $braces;
 	if ( $type eq '$' ) {
+
+		# If it is cast to '$' or '@', that trumps any braces
+		my $before = $self->sprevious_sibling;
+		return $symbol if $before &&
+			$before->isa( 'PPI::Token::Cast' ) &&
+			$cast_which_trumps_braces{ $before->content };
+
+		# Otherwise the braces rule
 		substr( $symbol, 0, 1, '@' ) if $braces eq '[]';
 		substr( $symbol, 0, 1, '%' ) if $braces eq '{}';
 
