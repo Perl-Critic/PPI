4ad0d326cd781b4004ef3b8e6cd809c91318e3fe
Mike O'Regan <moregan@stresscafe.com>
2014-03-02 17:30:59 -0600

    tests and fix for methods/functions like ->x86_convert() parsing as containing the x operator

--- lib/PPI/Token/Whitespace.pm
+++ lib/PPI/Token/Whitespace.pm
@@ -151,6 +151,17 @@ BEGIN {
 	};
 }
 
+# The x operator cannot follow most Perl operators, implying that
+# anything beginning with x following an operator is a word.
+# These are the exceptions.
+my %X_CAN_FOLLOW_OPERATOR = map { $_ => 1 } qw( -- ++ );
+
+# The x operator cannot follow most structure elements, implying that
+# anything beginning with x following a structure element is a word.
+# These are the exceptions.
+my %X_CAN_FOLLOW_STRUCTURE = map { $_ => 1 } qw( } ] \) );
+
+
 sub __TOKENIZER__on_line_start {
 	my $t    = $_[1];
 	my $line = $t->{line};
@@ -380,14 +391,25 @@ sub __TOKENIZER__on_char {
 		return 'Operator';
 
 	} elsif ( $char == 120 ) { # $char eq 'x'
-                # x followed immediately by a digit is an operator, not a
-                # word.
-                #
-                # x followed immediately by '=' is the 'x=' operator, not
-                # 'x ='. An important exception is '=>', which makes the x
-                # into a bareword.
-                my $remainder = substr $t->{line}, $t->{line_cursor} + 1;
-                return 'Operator' if $remainder =~ /^(\d|=(?!>))/;
+		# x followed immediately by a digit can be the x
+		# operator or a word.  Disambiguate by checking
+		# whether the previous token is an operator that cannot be
+		# followed by the x operator, e.g.: +.
+		#
+		# x followed immediately by '=' is the 'x=' operator, not
+		# 'x ='. An important exception is x followed immediately by
+		# '=>', which makes the x into a bareword.
+		my $remainder = substr $t->{line}, $t->{line_cursor} + 1;
+		if ( $remainder =~ /^(?:\d|(?!(=>|[\w\s])))/ ) {
+			my $prev = $t->_last_significant_token;
+			if (
+				$prev
+				&& (!$prev->isa('PPI::Token::Operator') || $X_CAN_FOLLOW_OPERATOR{$prev})
+				&& (!$prev->isa('PPI::Token::Structure') || $X_CAN_FOLLOW_STRUCTURE{$prev})
+			) {
+				return 'Operator';
+			}
+		}
 
 		# Otherwise, commit like a normal bareword
 		return PPI::Token::Word->__TOKENIZER__commit($t);
