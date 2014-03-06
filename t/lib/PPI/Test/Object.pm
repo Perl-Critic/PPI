8743b59807213511110ee9055556de3d0f881a21
Christian Walde <walde.christian@googlemail.com>
2014-03-06 12:30:12 +0100

    reduce code duplication in PPI::Test::Object

--- t/lib/PPI/Test/Object.pm
+++ t/lib/PPI/Test/Object.pm
@@ -11,180 +11,136 @@ BEGIN {
 	$VERSION = '1.216_01';
 }
 
-
-
-
+sub r {
+	my ( $class, $tests, $code ) = @_;
+	Test::Object->register( class => $class, tests => $tests, code  => $code );
+}
 
 #####################################################################
 # PPI::Document Testing
 
-Test::Object->register(
-	class => 'PPI::Document',
-	tests => 1,
-	code  => \&document_ok,
+r(
+	'PPI::Document', 1, sub {
+		my $doc = shift;
+
+		# A document should have zero or more children that are either
+		# a statement or a non-significant child.
+		my @children = $doc->children;
+		my $good = grep {
+			_INSTANCE($_, 'PPI::Statement')
+			or (
+				_INSTANCE($_, 'PPI::Token') and ! $_->significant
+				)
+			} @children;
+
+		is( $good, scalar(@children),
+			'Document contains only statements and non-significant tokens' );
+
+		1;
+	}
 );
-
-sub document_ok {
-	my $doc = shift;
-
-	# A document should have zero or more children that are either
-	# a statement or a non-significant child.
-	my @children = $doc->children;
-	my $good = grep {
-		_INSTANCE($_, 'PPI::Statement')
-		or (
-			_INSTANCE($_, 'PPI::Token') and ! $_->significant
-			)
-		} @children;
-
-	is( $good, scalar(@children),
-		'Document contains only statements and non-significant tokens' );
-
-	1;
-}
-
-
-
-
 
 #####################################################################
 # Are there an unknowns
 
-Test::Object->register(
-	class => 'PPI::Document',
-	tests => 3,
-	code  => \&unknown_objects,
+r(
+	'PPI::Document', 3, sub {
+		my $doc = shift;
+
+		is(
+			$doc->find_any('Token::Unknown'),
+			'',
+			"Contains no PPI::Token::Unknown elements",
+		);
+		is(
+			$doc->find_any('Structure::Unknown'),
+			'',
+			"Contains no PPI::Structure::Unknown elements",
+		);
+		is(
+			$doc->find_any('Statement::Unknown'),
+			'',
+			"Contains no PPI::Statement::Unknown elements",
+		);
+
+		1;
+	}
 );
-
-sub unknown_objects {
-	my $doc = shift;
-
-	is(
-		$doc->find_any('Token::Unknown'),
-		'',
-		"Contains no PPI::Token::Unknown elements",
-	);
-	is(
-		$doc->find_any('Structure::Unknown'),
-		'',
-		"Contains no PPI::Structure::Unknown elements",
-	);
-	is(
-		$doc->find_any('Statement::Unknown'),
-		'',
-		"Contains no PPI::Statement::Unknown elements",
-	);
-
-	1;
-}
-
-
-
-
 
 #####################################################################
 # Are there any invalid nestings?
 
-Test::Object->register(
-	class => 'PPI::Document',
-	tests => 1,
-	code  => \&nested_statements,
+r(
+	'PPI::Document', 1, sub {
+		my $doc = shift;
+
+		ok(
+			! $doc->find_any( sub {
+				_INSTANCE($_[1], 'PPI::Statement')
+				and
+				any { _INSTANCE($_, 'PPI::Statement') } $_[1]->children
+			} ),
+			'Document contains no nested statements',
+		);
+	}
 );
 
-sub nested_statements {
-	my $doc = shift;
+r(
+	'PPI::Document', 1, sub {
+		my $doc = shift;
 
-	ok(
-		! $doc->find_any( sub {
-			_INSTANCE($_[1], 'PPI::Statement')
-			and
-			any { _INSTANCE($_, 'PPI::Statement') } $_[1]->children
-		} ),
-		'Document contains no nested statements',
-	);	
-}
-
-Test::Object->register(
-	class => 'PPI::Document',
-	tests => 1,
-	code  => \&nested_structures,
+		ok(
+			! $doc->find_any( sub {
+				_INSTANCE($_[1], 'PPI::Structure')
+				and
+				any { _INSTANCE($_, 'PPI::Structure') } $_[1]->children
+			} ),
+			'Document contains no nested structures',
+		);
+	}
 );
 
-sub nested_structures {
-	my $doc = shift;
+r(
+	'PPI::Document', 1, sub {
+		my $doc = shift;
 
-	ok(
-		! $doc->find_any( sub {
-			_INSTANCE($_[1], 'PPI::Structure')
-			and
-			any { _INSTANCE($_, 'PPI::Structure') } $_[1]->children
-		} ),
-		'Document contains no nested structures',
-	);
-}
-
-Test::Object->register(
-	class => 'PPI::Document',
-	tests => 1,
-	code  => \&no_attribute_in_attribute,
+		ok(
+			! $doc->find_any( sub {
+				_INSTANCE($_[1], 'PPI::Token::Attribute')
+				and
+				! exists $_[1]->{_attribute}
+			} ),
+			'No ->{_attribute} in PPI::Token::Attributes',
+		);
+	}
 );
-
-sub no_attribute_in_attribute {
-	my $doc = shift;
-
-	ok(
-		! $doc->find_any( sub {
-			_INSTANCE($_[1], 'PPI::Token::Attribute')
-			and
-			! exists $_[1]->{_attribute}
-		} ),
-		'No ->{_attribute} in PPI::Token::Attributes',
-	);
-}
-
-
-
-
 
 #####################################################################
 # PPI::Statement Tests
 
-Test::Object->register(
-	class => 'PPI::Document',
-	tests => 1,
-	code  => \&valid_compound_type,
+r(
+	'PPI::Document', 1, sub {
+		my $document = shift;
+		my $compound = $document->find('PPI::Statement::Compound');
+		is(
+			scalar( grep { not defined $_->type } @$compound ),
+			0, 'All compound statements have defined ->type',
+		);
+	}
 );
-
-sub valid_compound_type {
-	my $document = shift;
-	my $compound = $document->find('PPI::Statement::Compound');
-	is(
-		scalar( grep { not defined $_->type } @$compound ),
-		0, 'All compound statements have defined ->type',
-	);
-}
-
-
-
-
 
 #####################################################################
 # Does ->location work properly
 # As an aside, fixes #23788: PPI::Statement::location() returns undef for C<({})>.
 
-Test::Object->register(
-	class => 'PPI::Document',
-	tests => 1,
-	code   => \&defined_location,
+r(
+	'PPI::Document', 1, sub {
+		my $document = shift;
+		my $bad      = $document->find( sub {
+			not defined $_[1]->location
+		} );
+		is( $bad, '', '->location always defined' );
+	}
 );
-
-sub defined_location {
-	my $document = shift;
-	my $bad      = $document->find( sub {
-		not defined $_[1]->location
-	} );
-	is( $bad, '', '->location always defined' );
-}
-
 
 1;
