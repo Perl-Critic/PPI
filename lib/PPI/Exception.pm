2b9a9a46ccb1150e577619e19ef38e5a0820649e
Mike O'Regan <moregan@stresscafe.com>
2014-12-23 21:15:06 -0600

    document the PPI::Exception class

--- lib/PPI/Exception.pm
+++ lib/PPI/Exception.pm
@@ -1,5 +1,26 @@
 package PPI::Exception;
 
+=head1 NAME
+
+PPI::Exception - The PPI exception base class
+
+=head1 SYNOPSIS
+
+  use PPI::Exception;
+  
+  my $e = PPI::Exception->new( 'something happened' );
+  $e->throw;
+
+  PPI::Exception->new( message => 'something happened' )->throw;
+  PPI::Exception->throw( message => 'something happened' );
+
+=head1 DESCRIPTION
+
+All exceptions thrown from within PPI will be instances or derivations
+of this class.
+
+=cut
+
 use strict;
 use Params::Util qw{_INSTANCE};
 
@@ -9,11 +30,17 @@ BEGIN {
 }
 
 
+=head1 METHODS
 
+=head2 new $message | message => $message, ...
 
+Constructs and returns a new C<PPI::Exception> object.
 
-#####################################################################
-# Constructor and Accessors
+A message for the exception can be passed, either as a string
+or as C<< message => $message >>. The message is available via the
+C<message> method.
+
+=cut
 
 sub new {
 	my $class = shift;
@@ -22,20 +49,19 @@ sub new {
 	return bless { message => 'Unknown Exception' }, $class;
 }
 
-sub message {
-	$_[0]->{message};
-}
 
-sub callers {
-	@{ $_[0]->{callers} || [] };
-}
+=head2 throw
 
+If called on a C<PPI::Exception> object, throws the object.
+If called on the class name, uses the arguments to construct a
+C<PPI::Exception> and then throw it.
 
+Each time the object is thrown, information from the Perl <caller(0)>
+call is saved and made available via the C<callers> method.
 
+This method never returns.
 
-
-#####################################################################
-# Main Methods
+=cut
 
 sub throw {
 	my $it = shift;
@@ -56,5 +82,30 @@ sub throw {
 	}
 	die $it;
 }
+
+
+=head2 message
+
+Returns the exception message passed to the object's constructor,
+or a default message.
+
+=cut
+
+sub message {
+	$_[0]->{message};
+}
+
+
+=head2 callers
+
+Returns a listref, each element of which is a listref of C<caller(0)>
+information.  The returned listref can be empty.
+
+=cut
+
+sub callers {
+	@{ $_[0]->{callers} || [] };
+}
+
 
 1;
