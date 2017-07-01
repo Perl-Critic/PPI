39e65ac744003b939677e3f9fa23b3642f8c7147
Christian Walde <walde.christian@gmail.com>
2017-07-01 04:02:37 +0200

    remove the last usage of base.pm

--- t/lib/Helper.pm
+++ t/lib/Helper.pm
@@ -1,10 +1,13 @@
 package Helper;
 
-use base 'Exporter';
+use strict;
+use warnings;
+use Exporter ();
 
 our $VERSION = '1.236';
 
-@EXPORT_OK = qw( check_with );
+our @ISA = "Exporter";
+our @EXPORT_OK = qw( check_with );
 
 sub check_with {
     my ( $code, $checker ) = @_;
