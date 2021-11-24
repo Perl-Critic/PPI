7d4417d78f3653e6f6fdf9eaa6731436950c7695
nanto_vi <nanto@moon.email.ne.jp>
2021-11-25 00:17:27 +0900

    Return correct name for lexical subroutines

--- lib/PPI/Statement/Sub.pm
+++ lib/PPI/Statement/Sub.pm
@@ -75,7 +75,8 @@ sub name {
 	my ($self) = @_;
 
 	# Usually the second token is the name.
-	my $token = $self->schild(1);
+	# The third token is the name if this is a lexical subroutine.
+	my $token = $self->schild(defined $self->type ? 2 : 1);
 	return $token->content
 	  if defined $token and $token->isa('PPI::Token::Word');
 
